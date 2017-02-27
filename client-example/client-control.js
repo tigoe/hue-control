6fd34f59dad85e40ffb2e2c0c28926d695e104e3
tigoe <tom.igoe@gmail.com>
2017-02-27 16:36:48 -0500

    updated client-control

--- client-example/client-control.js
+++ client-example/client-control.js
@@ -1,5 +1,5 @@
-var url = "128.122.151.161";           // the hub IP addres
-var username = "Ytn40SK1LEyg34AnGR95pfOJIolVZEs3Jw1cxOIw"       // fill in your Hub-given username here
+var url = "";           // the hub IP addres
+var username = ""       // fill in your Hub-given username here
 var usernameField;
 var addressField;
 var controlArray = new Array(); // array of light control divs
